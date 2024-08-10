51c039bf83275f1a3db9bc683b36c558b436584d
AndreyTara <explorituse@gmail.com>
2024-08-10 22:26:25 +0300

    'adds-opration'

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# npm i axios
+
+# npm i @reduxjs/toolkit + redux
+
 # React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
---
+++ src/redux/tasks/operations.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+import {
+  fetchingInProgress,
+  fetchingSuccess,
+  fetchingError,
+} from "../tasks/taskSlice";
+axios.defaults.baseURL = "https://62584f320c918296a49543e7.mockapi.io";
+
+const url = "https://62584f320c918296a49543e7.mockapi.io";
+export const fetchTasks = () => async (dispatch) => {
+  try {
+    // Індикатор завантаження
+    dispatch(fetchingInProgress());
+
+    // HTTP-запит
+    const response = await axios.get("/tasks");
+
+    // Обробка даних
+    dispatch(fetchingSuccess(response.data));
+  } catch (e) {
+    // Обробка помилки
+    dispatch(fetchingError(e.message));
+  }
+};
+
+// src/redux/operations.js
+
+// import axios from "axios";
+
+// axios.defaults.baseURL = "https://62584f320c918296a49543e7.mockapi.io";
+
+// const fetchTasks = () => async (dispatch) => {
+//   try {
+//     const response = await axios.get("/tasks");
+//   } catch (e) {}
+// };
